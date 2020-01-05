8be99e633e74376deb1b2bd148a8bf282e13e3ef
Stephanie Skinner <stephskinner87@gmail.com>
2020-01-05 19:30:29 +0000

    general

---
+++ Computing Coursework/neighbourDATA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computing_Coursework
+{
+    class neighbourDATA
+    {
+        public static Neighbour[] libAllNeighbs = new Neighbour[0]; // need access from other forms
+    }
+}
---
+++ Computing Coursework/propertyDATA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computing_Coursework
+{
+    class propertyDATA
+    {
+        public static Property[] libAllProperties = new Property[0]; // need access from other forms
+    }
+}
