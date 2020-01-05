1bd3f117da5727b23ed2c1a1321c9ba793768db4
Stephanie Skinner <stephskinner87@gmail.com>
2020-01-05 22:31:13 +0000

    general

--- Computing Coursework/addDis.Designer.cs
+++ Computing Coursework/addDis.Designer.cs
@@ -29,7 +29,29 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(addDis));
+            this.TXTdistname = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // TXTdistname
+            // 
+            this.TXTdistname.AutoSize = true;
+            this.TXTdistname.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(200)))), ((int)(((byte)(255)))));
+            this.TXTdistname.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXTdistname.Location = new System.Drawing.Point(12, 33);
+            this.TXTdistname.Name = "TXTdistname";
+            this.TXTdistname.Size = new System.Drawing.Size(117, 22);
+            this.TXTdistname.TabIndex = 0;
+            this.TXTdistname.Text = "District Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(200)))), ((int)(((byte)(255)))));
+            this.textBox1.Location = new System.Drawing.Point(149, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // addDis
             // 
@@ -37,19 +59,25 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(220)))), ((int)(((byte)(255)))));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(764, 450);
+            this.ClientSize = new System.Drawing.Size(400, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TXTdistname);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "addDis";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.TopMost = true;
             this.Load += new System.EventHandler(this.addDis_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label TXTdistname;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- Computing Coursework/addDis.cs
+++ Computing Coursework/addDis.cs
@@ -21,5 +21,10 @@ namespace Computing_Coursework
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
