afd8cafb4d139914eec4fff69fd2fb07ec7cf063
Stephanie Skinner <stephskinner87@gmail.com>
2020-01-06 00:59:15 +0000

    add neighbs

---
+++ Computing Coursework/AddNeighb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Computing_Coursework
+{
+    public partial class AddNeighb : Form
+    {
+        public AddNeighb()
+        {
+            InitializeComponent();
+        }
+    }
+}
